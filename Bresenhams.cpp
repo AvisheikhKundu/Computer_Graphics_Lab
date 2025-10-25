e24bfecd3559f452392161fd814f2f915bb4ac0e
Avisheikh Kundu <avisheikh76@gmail.com>
2025-10-25 09:44:27 +0600

    add new code

--- Bresenhams.cpp
+++ Bresenhams.cpp
@@ -1,131 +1,63 @@
 #include <windows.h>
 #include <GL/glut.h>
-#include <cstdlib>
+#include <math.h>
 
 void bresenhamsLine(int x1, int y1, int x2, int y2) {
-    float m = (float)(y2 - y1) / (float)(x2 - x1);
+    if (x1 > x2) { int tx = x1, ty = y1; x1 = x2; y1 = y2; x2 = tx; y2 = ty; }
+    float m = (x2 - x1) == 0 ? 999 : (float)(y2 - y1) / (float)(x2 - x1);
     int pk, x, y;
-
     if (m >= 0 && m <= 1) {
-        pk = (2 * (y2 - y1)) - (x2 - x1);
-        x = x1;
-        y = y1;
-        glBegin(GL_POINTS);
-        glColor3f(1.0f, 0.0f, 0.0f);
-        for (int i = 0; i < x2 - x1; i++) {
-            glVertex2f((float)x, (float)y);
-            if (pk < 0) {
-                pk = pk + 2 * (y2 - y1);
-            }
-            else {
-                pk = pk + 2 * (y2 - y1) - 2 * (x2 - x1);
-                y = y + 1;
-            }
-            x = x + 1;
-        }
+        int dx = x2 - x1, dy = y2 - y1; pk = 2 * dy - dx; x = x1; y = y1;
+        glBegin(GL_POINTS); glColor3f(1, 0, 0);
+        for (int i = 0;i < dx;i++) { glVertex2f(x, y); if (pk < 0) pk += 2 * dy; else { pk += 2 * dy - 2 * dx; y++; } x++; }
         glEnd();
     }
     else if (m > 1) {
-        pk = (2 * (x2 - x1)) - (y2 - y1);
-        x = x1;
-        y = y1;
-        glBegin(GL_POINTS);
-        glColor3f(0.0f, 1.0f, 0.0f);
-        for (int i = 0; i < y2 - y1; i++) {
-            glVertex2f((float)x, (float)y);
-            if (pk < 0) {
-                pk = pk + 2 * (x2 - x1);
-            }
-            else {
-                pk = pk + 2 * (x2 - x1) - 2 * (y2 - y1);
-                x = x + 1;
-            }
-            y = y + 1;
-        }
+        int dx = x2 - x1, dy = y2 - y1; pk = 2 * dx - dy; x = x1; y = y1;
+        glBegin(GL_POINTS); glColor3f(1, 0, 0);
+        for (int i = 0;i < dy;i++) { glVertex2f(x, y); if (pk < 0) pk += 2 * dx; else { pk += 2 * dx - 2 * dy; x++; } y++; }
         glEnd();
     }
     else if (m < 0 && m >= -1) {
-        pk = (2 * (y1 - y2)) + (x2 - x1);
-        x = x1;
-        y = y1;
-        glBegin(GL_POINTS);
-        glColor3f(0.0f, 0.0f, 1.0f);
-        for (int i = 0; i < abs(x2 - x1); i++) {
-            glVertex2f((float)x, (float)y);
-            if (pk < 0) {
-                pk = pk + 2 * (y1 - y2);
-            }
-            else {
-                pk = pk + 2 * (y1 - y2) + 2 * (x2 - x1);
-                y = y - 1;
-            }
-            x = x + 1;
-        }
+        int dx = x2 - x1, dy = y1 - y2; pk = 2 * dy - dx; x = x1; y = y1;
+        glBegin(GL_POINTS); glColor3f(1, 0, 0);
+        for (int i = 0;i < dx;i++) { glVertex2f(x, y); if (pk < 0) pk += 2 * dy; else { pk += 2 * dy - 2 * dx; y--; } x++; }
         glEnd();
     }
     else if (m < -1) {
-        pk = (2 * (x1 - x2)) + (y2 - y1);
-        x = x1;
-        y = y1;
-        glBegin(GL_POINTS);
-        glColor3f(1.0f, 1.0f, 0.0f);
-        for (int i = 0; i < abs(y2 - y1); i++) {
-            glVertex2f((float)x, (float)y);
-            if (pk < 0) {
-                pk = pk + 2 * (x1 - x2);
-            }
-            else {
-                pk = pk + 2 * (x1 - x2) + 2 * (y2 - y1);
-                x = x + 1;
-            }
-            y = y - 1;
-        }
+        int dx = x2 - x1, dy = y1 - y2; pk = 2 * dx - dy; x = x1; y = y1;
+        glBegin(GL_POINTS); glColor3f(1, 0, 0);
+        for (int i = 0;i < dy;i++) { glVertex2f(x, y); if (pk < 0) pk += 2 * dx; else { pk += 2 * dx - 2 * dy; x++; } y--; }
         glEnd();
     }
 }
 
 void display() {
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0, 1, 0);
+    glBegin(GL_POLYGON);
+    glVertex2f(0, 150);
+    glVertex2f(150, 0);
+    glVertex2f(0, -150);
+    glVertex2f(-150, 0);
+    glEnd();
 
-    // Reset projection and modelview matrices so repeated calls to display()
-    // won't accumulate additional transforms (which caused the diamond to shrink
-    // on every redisplay/click).
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(-500.0, 500.0, -500.0, 500.0, -500.0, 500.0);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-
-    // (reshape is defined at file scope below)
-    bresenhamsLine(-400, 0, 0, 400);
-    bresenhamsLine(0, 400, 400, 0);
-    bresenhamsLine(-400, 0, 0, -400);
-    bresenhamsLine(0, -400, 400, 0);
+    bresenhamsLine(0, 150, 150, 0);
+    bresenhamsLine(150, 0, 0, -150);
+    bresenhamsLine(0, -150, -150, 0);
+    bresenhamsLine(-150, 0, 0, 150);
 
     glFlush();
-}
-
-// Reshape callback: set viewport and projection when the window size changes.
-void reshape(int w, int h) {
-    if (h == 0) h = 1;
-    glViewport(0, 0, w, h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    // Keep a fixed orthographic volume so the drawn coordinates map consistently.
-    glOrtho(-500.0, 500.0, -500.0, 500.0, -500.0, 500.0);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
 }
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(800, 500);
-    glutInitWindowPosition(50, 50);
-    glutCreateWindow("Applying Bresenham's Line Drawing Algorithm");
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("Diamond - Bresenham");
+    glClearColor(0, 0, 0, 1);
+    glOrtho(-300, 300, -300, 300, -1, 1);
     glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
     glutMainLoop();
     return 0;
 }
---
+++ New2.cpp
@@ -0,0 +1,186 @@
+#include <windows.h>
+#include <GL/glut.h>
+#include <math.h>
+
+void bresenhamsLine(int x1, int y1, int x2, int y2) {
+    if (x1 > x2) {
+        int tempX = x1;
+        int tempY = y1;
+        x1 = x2;
+        y1 = y2;
+        x2 = tempX;
+        y2 = tempY;
+    }
+
+    float m = (x2 - x1) == 0 ? 999 : (float)(y2 - y1) / (float)(x2 - x1);
+    int pk, x, y;
+
+    if (m >= 0 && m <= 1) {
+        int dx = x2 - x1;
+        int dy = y2 - y1;
+        pk = (2 * dy) - dx;
+        x = x1;
+        y = y1;
+        glBegin(GL_POINTS);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        for (int i = 0; i < dx; i++) {
+            glVertex2f(x, y);
+            if (pk < 0)
+                pk = pk + 2 * dy;
+            else {
+                pk = pk + 2 * dy - 2 * dx;
+                y = y + 1;
+            }
+            x = x + 1;
+        }
+        glEnd();
+    }
+
+    else if (m > 1) {
+        int dx = x2 - x1;
+        int dy = y2 - y1;
+        pk = (2 * dx) - dy;
+        x = x1;
+        y = y1;
+        glBegin(GL_POINTS);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        for (int i = 0; i < dy; i++) {
+            glVertex2f(x, y);
+            if (pk < 0)
+                pk = pk + 2 * dx;
+            else {
+                pk = pk + 2 * dx - 2 * dy;
+                x = x + 1;
+            }
+            y = y + 1;
+        }
+        glEnd();
+    }
+
+    else if (m < 0 && m >= -1) {
+        int dx = x2 - x1;
+        int dy = y1 - y2;
+        pk = (2 * dy) - dx;
+        x = x1;
+        y = y1;
+        glBegin(GL_POINTS);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        for (int i = 0; i < dx; i++) {
+            glVertex2f(x, y);
+            if (pk < 0)
+                pk = pk + 2 * dy;
+            else {
+                pk = pk + 2 * dy - 2 * dx;
+                y = y - 1;
+            }
+            x = x + 1;
+        }
+        glEnd();
+    }
+
+    else if (m < -1) {
+        int dx = x2 - x1;
+        int dy = y1 - y2;
+        pk = (2 * dx) - dy;
+        x = x1;
+        y = y1;
+        glBegin(GL_POINTS);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        for (int i = 0; i < dy; i++) {
+            glVertex2f(x, y);
+            if (pk < 0)
+                pk = pk + 2 * dx;
+            else {
+                pk = pk + 2 * dx - 2 * dy;
+                x = x + 1;
+            }
+            y = y - 1;
+        }
+        glEnd();
+    }
+}
+
+void display() {
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-500.0, 500.0, -500.0, 500.0, -500.0, 500.0);
+    glMatrixMode(GL_MODELVIEW);
+
+    // --- Fill Star using 8 GL_POLYGON triangles ---
+    glColor3f(0.0f, 1.0f, 0.0f); // Green color for fill
+
+    glBegin(GL_POLYGON);
+    glVertex2f(0, 0);
+    glVertex2f(0, 300);
+    glVertex2f(100, 100);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glVertex2f(0, 0);
+    glVertex2f(100, 100);
+    glVertex2f(300, 0);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glVertex2f(0, 0);
+    glVertex2f(300, 0);
+    glVertex2f(100, -100);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glVertex2f(0, 0);
+    glVertex2f(100, -100);
+    glVertex2f(0, -300);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glVertex2f(0, 0);
+    glVertex2f(0, -300);
+    glVertex2f(-100, -100);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glVertex2f(0, 0);
+    glVertex2f(-100, -100);
+    glVertex2f(-300, 0);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glVertex2f(0, 0);
+    glVertex2f(-300, 0);
+    glVertex2f(-100, 100);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glVertex2f(0, 0);
+    glVertex2f(-100, 100);
+    glVertex2f(0, 300);
+    glEnd();
+
+    // --- Draw Bresenham's Outline ---
+    bresenhamsLine(0, 300, 100, 100);
+    bresenhamsLine(100, 100, 300, 0);
+    bresenhamsLine(300, 0, 100, -100);
+    bresenhamsLine(100, -100, 0, -300);
+    bresenhamsLine(0, -300, -100, -100);
+    bresenhamsLine(-100, -100, -300, 0);
+    bresenhamsLine(-300, 0, -100, 100);
+    bresenhamsLine(-100, 100, 0, 300);
+
+    glFlush();
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(50, 50);
+    glutCreateWindow("Star - Bresenham Outline");
+    glutDisplayFunc(display);
+    glutMainLoop();
+
+    return 0;
+}
